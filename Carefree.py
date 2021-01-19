aede1df494eb19a5b681b4b0a4a71c1bf531c965
Neek0tine <nicholas.juan.kalvin-2020@stmm.unair.ac.id>
2021-01-19 13:19:24 +0700

    Major changes, file name change and overall reform.

--- Carefree.py
+++ Carefree.py
@@ -1,8 +1,11 @@
+import config
+import random
 import time
+import os
+from urllib.request import urlretrieve
 from msedge.selenium_tools import Edge, EdgeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+
+#  TODO: ERROR 216, FIX, NOW
 
 print('==============================================================================================')
 print('=  ________      ________      ________  ________  ___           _______       ________      =')
@@ -12,111 +15,171 @@ print('=  \ \   _  _\   \ \   __  \   \ \   __\ \ \   __\ \ \  \        \ \  \_|
 print('=   \ \  \\  \|    \ \  \ \  \   \ \  \_|  \ \  \_|  \ \  \____    \ \  \_|\ \   \ \  \\  \|   =')
 print('=    \ \__\\ _\     \ \__\ \__\   \ \__\    \ \__\    \ \_______\   \ \_______\   \ \__\\ _\   =')
 print('=     \|__|\|__|    \|__|\|__|    \|__|     \|__|     \|_______|    \|_______|    \|__|\|__| =')
-print('=                Scrap TF Raffler                                                            =')
+print('=                Scrap TF Raffler       V2.0                                                 =')
 print('=                           by Neek0tine                                                     =')
 print('==============================================================================================')
 
-# Initialize WebDriver
-options = EdgeOptions()
-options.use_chromium = True
-options.add_argument('user-data-dir=C:\\Users\\nicho\\AppData\\Local\\Microsoft\\Edge\\TEST')
-options.add_argument('profile-directory=Profile 2')
-# options.add_argument('--headless')
-
-driver = Edge(options=options, executable_path='C:\\Users\\nicho\\Documents\\Program Data\\msedgedriver.exe')
+tolerance = config.tolerance
+delay = config.delay
+headless = config.headless
+chk_frq = config.chk_frq
 
 
-# Web scraping available raffles
-def get_data():
-    raffle_count = 0
-    try:
-        driver.get('https://scrap.tf/raffles')
-        driver.minimize_window()
-        stat = driver.find_element_by_tag_name('h1').text
-        stat = str(stat)
-        raffle_count = stat.split('/')
-        joined_count = int(raffle_count.pop(0))
-        raffle_count = "".join(raffle_count)
-        raffle_count = int(raffle_count)
-        raffle_count = raffle_count
+def initialize():  # Initialize the webdriver engine
+    options = EdgeOptions()
+    options.use_chromium = True
+    if headless is False:
+        pass
+    else:
+        options.add_argument(argument='--headless')
 
-        if joined_count == raffle_count - 1:
+    def get_engine():
+        localappdata = os.getenv('LOCALAPPDATA')
+        install_dir = str(localappdata + '\\Microsoft\\Edge\\Auto-raffler')
+        files_list = os.listdir(install_dir)
 
-            def overseer():
-                print('[!] Entered all raffles! Enabling overseer mode!')
-                time.sleep(20)
-                driver.get('https://scrap.tf/raffles')
-
-                while True:
-                    stat1 = driver.find_element_by_tag_name('h1').text
-                    stats = str(stat1)
-                    print(f'[+] Raffle count : {stats}')
-                    raffle_count_ov = stat.split('/')
-                    if raffle_count_ov[0] > raffle_count_ov[-1]:
-                        get_data()
-                    else:
-                        time.sleep(20)
-                        driver.refresh()
-
-            overseer()
-
-        else:
+        if 'msedgedriver.exe' in files_list:
             pass
-        print(f'[+] Raffle count : {stat}')
-
-    except:
-        print('[!] Error: Failed to open site')
-
-    links = []
-
-    def collect_data():
-
-        print('[+] Collecting raffles data ...')
-
-        def pick():
-            elems = driver.find_elements_by_class_name("raffle-name [href]")
-            for elem in elems:
-                href = elem.get_attribute('href')
-                if href in links:
-                    pass
+        else:
+            print('[!] Browser engine not found, downloading ...')
+            ms_site = 'https://msedgedriver.azureedge.net/87.0.664.75/edgedriver_win32.zip'
+            destination = str(install_dir + '\\msedgedriver.exe')
+            urlretrieve(ms_site, destination)
+            y = 0
+            while y < 100:
+                time.sleep(2)
+                y += 10 + random.randint(1, 6)
+                if y > 100:
+                    print("\r Download progress :  100 %")
+                    break
                 else:
-                    links.append(href)
+                    print("\r Download progress :  {}".format(y), '%', end="")
 
-        pick()
+    get_engine()
 
-        while len(links) < raffle_count:
-            print(f'[+] Collected {len(links)} data, trying to get more ... ')
-            if len(links) > raffle_count - 5:
-                driver.refresh()
-                pick()
-                break
-            else:
-                time.sleep(1)
+    def get_profile():
+        if 'Auto-raffler' in os.listdir('C:\\Users\\nicho\\AppData\\Local\\Microsoft\\Edge'):
+            options.add_argument('user-data-dir=C:\\Users\\nicho\\AppData\\Local\\Microsoft\\Edge\\Auto-raffler')
+            options.add_argument('profile-directory=Profile 1')
+        else:
+            os.mkdir('C:\\Users\\nicho\\AppData\\Local\\Microsoft\\Edge\\Auto-raffler')
+            get_profile()
+
+    get_profile()
+
+    driver = Edge(options=options,
+                  executable_path='C:\\Users\\nicho\\AppData\\Local\\Microsoft\\Edge\\Auto-raffler\\msedgedriver.exe')
+    """
+    try:
+        driver = Edge(options=options,
+                      executable_path='C:\\Users\\nicho\\AppData\\Local\\Microsoft\\Edge\\Auto-raffler\\msedgedriver.exe')
+    except OSError:
+        os.remove(os.getenv('LOCALAPPDATA')+'\\Microsoft\\Edge\\Auto-raffler\\msedgedriver.exe')
+        print()
+        print('[!] Unsupported browser engine! Please check the readme file on how to fix this!')
+        exit()
+    driver = Edge(options=options,
+                  executable_path='C:\\Users\\nicho\\AppData\\Local\\Microsoft\\Edge\\Auto-raffler\\msedgedriver.exe')
+    """
+
+    def get_stat():
+        # Get raffle info
+        driver.get("https://scrap.tf/raffles")
+        try:
+            stat = driver.find_element_by_tag_name('h1').text
+            stat = str(stat)
+            stat = stat.split('/')
+            entered_count = stat[0]
+            entered_count = int(entered_count)
+            total_count = stat[-1]
+            total_count = int(total_count)
+            available_count = total_count - entered_count
+
+            print(f'[+] You have entered : {entered_count} raffles')
+            print(f'[+] Raffles available : {total_count} raffles')
+            print(f'[+] Raffles to be joined : {available_count} raffles\n')
+            return [entered_count, total_count, available_count]
+        except:
+            print('Unable to get website data. Are you logged in?')
+            driver.find_element_by_class_name('sits-login').click()
+            cfm = ''
+            while cfm != 'y':
+                print(
+                    'Feel free to login or not to login. This program saves its data on your local laptop and is not connected to the internet')
+                cfm = str(input('Have you logged in? (Y/N) : '))
+                cfm = cfm.casefold()
+
+    def get_links():  # Collecting raffle links
+        stat = get_stat()
+        joined = []
+        available = []
+        total = []
+
+        while len(joined) < stat[0] - tolerance and len(available) != stat[2]:
+            z = 0
+            while driver.find_element_by_class_name('panel-body.raffle-pagination-done').text != "That's all, no more!":
+                z += 1
+                time.sleep(2)
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                pick()
-                time.sleep(1)
-        print(f'[+] Collected {len(links)} raffles data!')
+                if z == 12:
+                    driver.refresh()
 
-    collect_data()
+            joined_class = driver.find_elements_by_class_name("panel-raffle.raffle-entered [href]")
+            joined = [elem.get_attribute('href') for elem in joined_class]
+            total_class = driver.find_elements_by_class_name("panel-raffle  [href]")
+            total = [elem0.get_attribute('href') for elem0 in total_class]
 
-    n = len(links)
+            for x in joined:
+                if 'profile' in x:
+                    joined.remove(x)
+            for y in total:
+                if 'profile' in y:
+                    total.remove(y)
+
+            available = list(set(total) - set(joined))
+            print('[+] Getting raffle links ...')
+            print(f'[+] Collected {len(joined)} links to joined raffles')
+            print(f'[+] Collected {len(total)} links to all raffles')
+            print(f'[+] Collected {len(available)} links to join-able raffles')
+        print('\n[+] All raffle links collected!')
+
+        return joined, total, available
 
     def enter_raffle():
-        z = 1
-        for link in links:
-            driver.get(link)
-            print(f'Entering Raffle {z} of {n} ...')
+        joined, total, available = get_links()
+        for raffle in available:
+            print("\r {}".format(len(available)), 'Raffles left', end="")
+            driver.get(url=raffle)
+            desc = ''
             try:
-                wait = WebDriverWait(driver, 1)
-                wait.until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR,
-                                                    '#pid-viewraffle > div.container > div > div.well.raffle-well > div.row.raffle-opts-row > div.col-xs-7.enter-raffle-btns > button:nth-child(3)'))).click()
-                time.sleep(2.2)
+                driver.find_element_by_css_selector(
+                    '#pid-viewraffle > div.container > div > div.well.raffle-well > div.row.raffle-opts-row > div.col-xs-7.enter-raffle-btns > button:nth-child(3)').click()
             except:
-                pass
-            z += 1
+                try:
+                    desc = driver.find_element_by_class_name('raffle-row-full-width').text
+                except:
+                    print('[!]Uknown error occured, pleas contact the developer!')
+                if 'raffle ended' in desc:
+                    print('[!] A raffle has ended and I failed to join it on time!')
+
+            joined.append(raffle)
+            available.remove(raffle)
+            time.sleep(delay)
+
+    def overwatch():
+        while True:
+            stat = get_stat()
+            if stat[0] >= stat[1]:
+                print('\n[!] All raffles joined, monitoring new raffles ...')
+                print('====================================================')
+                time.sleep(chk_frq)
+                driver.refresh()
+            else:
+                enter_raffle()
 
     enter_raffle()
+    overwatch()
+    driver.quit()
 
 
-get_data()
+initialize()
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-tolerance = 2
-delay = 3.5
-headless = False
-chk_frq = 30
+tolerance = 2  # How many scrap.tf stats missmatch tolerated. 0 being a perfect match, will result in overflow if the website gives false data
+delay = 3.5  # How many seconds the program enter another raffle. Internet speed and browser load speed is taken into consideration (5 sec each raffle)
+headless = False  # Experimental, using this will surely bug the program
+chk_frq = 30  # The interval between refresh in active monitoring mode. (RECOMMENDED >5)
